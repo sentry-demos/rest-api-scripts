560e3a4a35839393ba50aab17b7915fbccf3976b
adam000034 <aatothfejel@dons.usfca.edu>
2022-02-02 12:39:43 -0800

    Member To Team Mapping Done

--- sentry_member_team_map.py
+++ sentry_member_team_map.py
@@ -12,25 +12,6 @@ class Sentry():
         self.org = org
         self.token = token
 
-    def _get_api_pagination(self, endpoint):
-        """HTTP GET the Sentry API, following pagination links"""
-
-        headers = {'Authorization': f'Bearer {self.token}'}
-
-        results = []
-        url = f'{self.base_url}{endpoint}'
-        next = True
-        while next:
-            response = requests.get(url, headers=headers)
-            results.extend(response.json())
-
-            url = response.links.get('next', {}).get('url')
-            next = response.links.get('next', {}).get('results') == 'true'
-            if url == None:
-                next = False
-
-        return results
-
     def _get_api(self, endpoint):
         """HTTP GET the Sentry API"""
 
@@ -55,33 +36,13 @@ class Sentry():
             'Content-Type': f'application/json'
         }
         url = f'{self.base_url}{endpoint}'
-        print("URL", url)
-        print("data", data)
-        print("headers", headers)
         return requests.patch(url, headers=headers, json=data)
-        p
-
-    def get_project_slugs(self):
-        """Return a list of project slugs in this Sentry org"""
-
-        results = self._get_api(f'/api/0/organizations/{self.org}/projects/')
-
-        return [project.get('slug', '') for project in results]
-
-    def get_keys(self, project_slug):
-        """return the public and secret DSN links for the given project slug"""
-
-        results = self._get_api(f'/api/0/projects/{self.org}/{project_slug}/keys/')
-
-        return (results[0]['dsn']['public'], results[0]['dsn']['secret'])
 
     def get_teams(self):
         """Return a dictionary mapping team slugs to a set of project slugs"""
 
-        results = self._get_api(f'/api/0/organizations/{self.org}/teams/')
-
-        #return {team['id']: team for team in results if 'id' in team}
-        return {(team['slug'], team['id']): team for team in results if ('id' in team, 'slug' in team)}
+        results = self._get_api(f'/api/0/organizations/{self.org}/scim/v2/Groups')
+        return results.get("Resources")
 
     def get_team_members(self, teamId):
         """Return a dictionary mapping team slugs to member ids"""
@@ -90,85 +51,114 @@ class Sentry():
 
         return team
 
-    def create_team(self, name, slug):
-        """Create a new team in this Sentry org with the given name and slug"""
+    def get_org_members(self):
+        """Get members from an organization"""
 
-        return self._post_api(f'/api/0/organizations/{self.org}/teams/', data={'name': name, 'slug': slug})
+        members = self._get_api(f'/api/0/organizations/{self.org}/scim/v2/Users')
+        return members.get("Resources")
 
-    def give_team_access_to_project(self, team, project):
-        """Give a team access to a project"""
-
-        return self._post_api(f'/api/0/projects/{self.org}/{project}/teams/{team}/')
+    def create_team_member(self, data):
+        """Create team member"""
+        teammember = self._post_api(f'/api/0/organizations/{self.org}/scim/v2/Users', json.dumps(data))
+        return teammember.__dict__
 
     def update_team(self, members, teamId):
         """Update team attributes"""
         return self._patch_api(f'/api/0/organizations/{self.org}/scim/v2/Groups/' + teamId, data=
-        {
-            "schemas": [
-            "urn:ietf:params:scim:api:messages:2.0:PatchOp"
-            ],
+        {   "schemas": [
+                "urn:ietf:params:scim:api:messages:2.0:PatchOp"
+                    ],
             "Operations": [
-            {
+              {
                 "op": "replace",
                 "path": "members",
-                "value": [
-                    {
-                        "display": "email",
-                        "value": somevalue
-                    }
-                ]
-            }
+                "value": members
+              }
             ]
         }
-        )
-
-def get_team_projects(teams):
-    mapping = {}
-    for slug, team in teams.items():
-        mapping[slug] = {project['slug'] for project in team.get('projects', []) if 'slug' in project}
-
-    return mapping
+    )
 
 
 if __name__ == '__main__':
-    onpremise_token = os.environ['SENTRY_ONPREMISE_AUTH_TOKEN']
-    cloud_token = os.environ['SENTRY_CLOUD_AUTH_TOKEN']
 
     onpremise_SCIMToken = os.environ['SENTRY_ONPREMISE_SCIM_AUTH_TOKEN']
     cloud_SCIMToken = os.environ['SENTRY_CLOUD_SCIM_AUTH_TOKEN']
 
     # copy over onpremise url (e.g. http://sentry.yourcompany.com)
-    sentry_onpremise = Sentry('https://sentry.io',
-                              <ONPremiseOrg>,
-                              onpremise_token)
-
-    sentry_cloud = Sentry('https://sentry.io',
-                          <SentryCloudOrg>,
-                          cloud_token)
-
-    onpremise_SCIM = Sentry('https://sentry.io',
-                              <ONPremiseOrg>,
-                               onpremise_SCIMToken)
+    onpremise_SCIM = Sentry('<ON_PREMISE_URL>',
+                            '<ON_PREMISE_ORG_SLUG>',
+                            onpremise_SCIMToken)
 
     cloud_SCIM = Sentry('https://sentry.io',
-                          <SentryCloudOrg>,
-                          cloud_SCIMToken)
+                        '<ORG_SLUG>',
+                        cloud_SCIMToken)
 
-    onpremise_teams_ids = sentry_onpremise.get_teams()
-    cloud_teams_ids = sentry_cloud.get_teams()
+    onpremise_teams = onpremise_SCIM.get_teams()
+    cloud_teams = cloud_SCIM.get_teams()
+    updated_ids_dict = {}
 
-    # Teams exist in both spots, team names are assumed they exist in both spots but
+    #To be used for ID swap for easier lookup later
+    onpremmembers = onpremise_SCIM.get_org_members()
+    cloudmembers = cloud_SCIM.get_org_members()
+    
+
+    #get id of old account and store it along with email in a common dictionary
+
+    for member in onpremmembers:
+        found = 0
+        for cloudmember in cloudmembers:
+              if ((member.get('emails')[0].get('value')) == 
+                (cloudmember.get('emails')[0].get('value'))):
+                    member['id'] = cloudmember.get('id')
+                    updated_ids_dict[member.get('emails')[0].get('value')] = cloudmember.get('id')
+                    found=1
+                    break
+
+        #Make a new user if not found in new org, email must match what is on Okta
+        if (found == 0):
+
+            #Create new user
+            data = {
+                "schemas": ["urn:ietf:params:scim:schemas:core:2.0:User"],
+                "userName": "",
+                "name": {"givenName": "", "familyName": ""},
+                "emails": [{"primary": True, "value": "", "type": "work"}],
+                "active": True,
+            }
+            data["userName"] = member.get('emails')[0].get('value');
+            data.get("emails")[0]["value"] = member.get('emails')[0].get('value');
+            newuser = cloud_SCIM.create_team_member(data)
+            
+            #get id of new user
+            newuser_id = newuser.get("id")
+
+            #update id in onpremmembers array
+            updated_ids_dict[member.get('emails')[0].get('value')] = newuser_id
+
+    # By now, all onpremmembers should have updated ids for the new org,
+    # this will enable easy dictionary lookup for ids
+
+    # Teams exist in both spots, team names must exist in both spots but
     # ids will be different, as a result, team name must be used because
     # that's whats in common
 
-    for team in onpremise_teams_ids.keys():
-        members = onpremise_SCIM.get_team_members(team[1]).get("members")
-        #replace the team in the new org with all team member info on old org
+    # if a team exists in old org but not in new org, nothing will happen
+    for team in onpremise_teams:
 
-        #matching team in cloud sentry
-        for cloudTeam in cloud_teams_ids:
-            #there is a match on team name but ids will be different
-            if (cloudTeam[0] == team[0]):
-                print(cloud_SCIM.update_team(members, cloudTeam[1]))
-                break
+        #update old team members with ids of new org member ids
+        for member in team.get("members"):
+            #update ids here
+            member['value'] = updated_ids_dict.get(member.get('display'))
+
+        #by now all onpremise team members have updated ids
+
+        #matching onprem team with updated ids to cloud sentry
+        for cloudTeam in cloud_teams:
+
+            #there is a match on team name, onprem teams will have updated ids from
+            #cloud account
+            if (cloudTeam['displayName'] == team['displayName']):
+                cloud_SCIM.update_team(team.get("members"), cloudTeam['id'])
+
+
 
